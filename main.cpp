c236dc248f5722018ccdc7b8647af4ee3b638fc0
michael rokitko <alarn777@gmail.com>
2019-04-21 13:29:52 +0300

    textBox fixed (working)

--- main.cpp
+++ main.cpp
@@ -3,20 +3,20 @@
 
 int main(int argc, char const *argv[])
 {
-    Label l;
-    l.setText("Hello more World");
-    l.setPosition(11, 1);
-    l.draw();
+    // Label l;
+    // l.setText("Hello more World");
+    // l.setPosition(11, 1);
+    // l.draw(0);
 
-    Label g;
-    g.setPosition(15,2);
-    g.setText("Such label, so wow");
-    g.draw();
+    // Label g;
+    // g.setPosition(15,2);
+    // g.setText("Such label, so wow");
+    // g.draw(0);
 
     auto handle = GetStdHandle(STD_INPUT_HANDLE);
 
-    TextBox tb(12, 3, 3, "hello");
-    tb.draw();
+    TextBox tb(12, 3, 3, "12345");
+    tb.draw(-1);
     
     while (1)
     {
