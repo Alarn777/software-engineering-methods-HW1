2ee2c328a78ff67bd756d4865f07d844dcde8450
sworgkh <sworgkh@gmail.com>
2019-04-21 11:45:36 +0300

    comments

--- main.cpp
+++ main.cpp
@@ -3,16 +3,6 @@
 
 int main(int argc, char const *argv[])
 {
-    Label l;
-    l.setText("Hello more World");
-    l.setPosition(11, 1);
-    l.draw();
-
-    Label g;
-    g.setPosition(15,2);
-    g.setText("Such label, so wow");
-    g.draw();
-
     auto handle = GetStdHandle(STD_INPUT_HANDLE);
 
     TextBox tb(12, 3, 3, "hello");
--- textbox.cpp
+++ textbox.cpp
@@ -21,9 +21,11 @@ void TextBox::handleKeyboardEvent(KEY_EVENT_RECORD& event)
     {
         int textWidth = value.length();
         CONSOLE_SCREEN_BUFFER_INFO info;
+
         auto handle = GetStdHandle(STD_OUTPUT_HANDLE);
         GetConsoleScreenBufferInfo(handle, &info);
-        auto offset = info.dwCursorPosition.X - this->left - 1;
+        
+        auto offset = info.dwCursorPosition.X - this->left - 1; // absolute cursor position in textbox 
 
         if (offset > 0 && event.wVirtualKeyCode == VK_LEFT)
         {
@@ -51,29 +53,31 @@ void TextBox::handleKeyboardEvent(KEY_EVENT_RECORD& event)
 void TextBox::draw()
 {
     COORD coord = { left, top };
+    
     auto handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    
     SetConsoleCursorPosition(handle, coord);
     SetConsoleTextAttribute(handle, color | (background << 4));
     
-    std::cout << (char) 0xda;
-    for (int i=0; i<width; i++)
+    std::cout << (char) 0xda; // left top corner
+    for (int i=0; i<width; i++) //top line
         std::cout << (char) 0xc4;
-    std::cout << (char) 0xbf;
+    std::cout << (char) 0xbf; //right top corner
 
     SetConsoleCursorPosition(handle, {coord.X, coord.Y+1});
-    std::cout << (char) 0xb3;
+    std::cout << (char) 0xb3; //vertical line
     
     for (int i =0; i < width; i++)
         std::cout << (i >= value.length() ? ' ' : value[i]);
     
     SetConsoleCursorPosition(handle, { left + width + 1, top + 1 });
-    std::cout << (char) 0xb3;
+    std::cout << (char) 0xb3; //vertical line
 
     SetConsoleCursorPosition(handle, { left, top + 2 });
-    std::cout << (char) 0xc0;
-    for (int i = 0; i < width; i++)
+    std::cout << (char) 0xc0; // bottom left corner
+    for (int i = 0; i < width; i++) // bottom border
         std::cout << (char) 0xc4;
-    std::cout << (char) 0xd9;
+    std::cout << (char) 0xd9; //right bottom corner
 
     SetConsoleCursorPosition(handle, { left + 1 + value.length(), top+1 });
 
